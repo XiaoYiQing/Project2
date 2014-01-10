3e9a1977ed84782e6fceae196041ed7ae3e63d1a
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-01-10 14:59:01 -0500

    br 3 commit 2

--- de.vogella.git.first/src/GitTest.java
+++ de.vogella.git.first/src/GitTest.java
@@ -1,10 +1,15 @@
 
 public class GitTest {
 	public static void main(String[]args){
+		//This is a merge test btw master and br3 branches
+		
 		System.out.println("Git is FUN!");
 		System.out.println("Commit 2");
 		
 		System.out.println("This is exclusively" +
 				"within br3");
+		
+		System.out.println("Hopefully the merge with master by br3" +
+				"will not result in too much conflict");
 	}
 }
