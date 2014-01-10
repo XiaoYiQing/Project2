7cd7bffa534f6a8c9e5aec8f5d0b433d9b30cbb6
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-01-10 14:53:45 -0500

    br 3 commit 1

--- de.vogella.git.first/src/GitTest.java
+++ de.vogella.git.first/src/GitTest.java
@@ -3,5 +3,8 @@ public class GitTest {
 	public static void main(String[]args){
 		System.out.println("Git is FUN!");
 		System.out.println("Commit 2");
+		
+		System.out.println("This is exclusively" +
+				"within br3");
 	}
 }
